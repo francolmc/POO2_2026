cca45f1f34f96d3aa66e18102736286718b7611b
francolmc <34035571+francolmc@users.noreply.github.com>
2026-09-24 11:07:26 -0300

    buscar registro

--- clase8/main.py
+++ clase8/main.py
@@ -2,10 +2,12 @@ from models.libro import Libro
 from repositories.libro_repository import LibroRepositorio
 from database.connection import obtener_conexion, crear_tablas
 
-rayuela = Libro("978-0-2", "Rayuela", 2)
+# rayuela = Libro("978-0-2", "Rayuela", 2)
 
 crear_tablas()
 
 conexion = obtener_conexion()
 respositorio_libro = LibroRepositorio(conexion)
-respositorio_libro.crear(rayuela)
+# respositorio_libro.crear(rayuela)
+rayuela = respositorio_libro.buscar_por_isbn("978-0-2")
+print(rayuela.titulo, rayuela.copias_disponibles)
--- clase8/repositories/libro_repository.py
+++ clase8/repositories/libro_repository.py
@@ -11,4 +11,12 @@ class LibroRepositorio:
         """, (libro.isbn, libro.titulo, libro.copias_disponibles,))
         self.__conexion.commit()
 
-    
+    def buscar_por_isbn(self, isbn:str):
+        cursor = self.__conexion.cursor()
+        cursor.execute("""
+        SELECT isbn, titulo, copias_disponible FROM libros WHERE isbn = ?
+        """, (isbn,))
+        registro = cursor.fetchone()
+        if registro is None:
+            return None
+        return Libro(registro[0], registro[1], registro[2])
