d2c30779576c171c6d2b5293c86bb540cb627164
francolmc <34035571+francolmc@users.noreply.github.com>
2026-09-24 15:09:21 -0300

    Modelos y repositorios

--- clase8/main.py
+++ clase8/main.py
@@ -7,7 +7,12 @@ from database.connection import obtener_conexion, crear_tablas
 crear_tablas()
 
 conexion = obtener_conexion()
+
 respositorio_libro = LibroRepositorio(conexion)
 # respositorio_libro.crear(rayuela)
 rayuela = respositorio_libro.buscar_por_isbn("978-0-2")
 print(rayuela.titulo, rayuela.copias_disponibles)
+# rayuela.copias_disponibles = 5
+# respositorio_libro.actualizar_libro(rayuela)
+
+# respositorio_libro.eliminar("978-0-2")
--- clase8/repositories/libro_repository.py
+++ clase8/repositories/libro_repository.py
@@ -20,3 +20,21 @@ class LibroRepositorio:
         if registro is None:
             return None
         return Libro(registro[0], registro[1], registro[2])
+
+    def actualizar_libro(self, libro:Libro):
+        cursor = self.__conexion.cursor()
+        cursor.execute("""
+        UPDATE libros SET
+            titulo = ?,
+            copias_disponible = ?
+        WHERE
+            isbn = ?
+        """, (libro.titulo, libro.copias_disponibles, libro.isbn))
+        self.__conexion.commit()
+        return cursor.rowcount > 0
+
+    def eliminar(self, isbn):
+        cursor = self.__conexion.cursor()
+        cursor.execute("DELETE FROM libros WHERE isbn = ?", (isbn,))
+        self.__conexion.commit()
+        return cursor.rowcount > 0
